b56dbe7a16279d9805ea50fa6cce4606325b258d
matt190589 <matthew.doherty97@gmail.com>
2022-12-02 11:07:08 +0000

    Fetch and ID generator working

--- christmas-movies/src/components/Main/index.js
+++ christmas-movies/src/components/Main/index.js
@@ -1,35 +1,35 @@
-import React, { useState, useEffect } from 'React'
+import React, { useState, useEffect } from "react";
+import christmasMovieIDs from "../../data";
 
-const API_KEY = 'c8ce6acf'
-const API_KEY_BACKUP = '3e600c52'
-
-
-// useEffect(() => {
-//     async function callURL() {
-//       const response = await fetch(
-//         `http://localhost:3050/api/posts/?week=${count}`
-//       );
-//       const data = await response.json();
-//       setFetchData([...data.payload]);
-//     }
-//     callURL();
-//   }, [count]);
+const API_KEY = "c8ce6acf";
+const API_KEY_BACKUP = "3e600c52";
 
 function Main() {
-    const [movieData, setMovieData] = useState(null)
-    const [movieID, setMovieID] = useState(null)
-    
-    useEffect(() => {
-        let url = 'http://www.omdbapi.com/?i=' + movieID + '&apikey=' + API_KEY
-        async function callURL() {
-            const response = await fetch(url);
-            const data = await response.json();
-            setMovieData(data)
-        }
-        callURL()
-    }, [movieID])
+  const [movieData, setMovieData] = useState(null);
+  const [movieID, setMovieID] = useState(null);
 
-    return <div>
+  useEffect(() => {
+    let url = "http://www.omdbapi.com/?i=" + movieID + "&apikey=" + API_KEY;
+    async function callURL() {
+      const response = await fetch(url);
+      const data = await response.json();
+      setMovieData(data);
+      console.log(movieData);
+    }
+    callURL();
+  }, [movieID]);
 
+  function generatemovieID() {
+    let randMovieIDNum = Math.floor(Math.random() * christmasMovieIDs.length);
+    console.log(randMovieIDNum);
+    setMovieID(christmasMovieIDs[randMovieIDNum]);
+    console.log(movieID);
+  }
+
+  return (
+    <div>
+      <button onClick={generatemovieID}>Click for Movie ID</button>
     </div>
+  );
 }
+export default Main;
