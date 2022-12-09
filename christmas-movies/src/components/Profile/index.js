5caf5cad7e766bad62170325ebad944208eb078e
matt190589 <112311267+matt190589@users.noreply.github.com>
2022-12-09 11:49:28 +0000

    Small adjustment to access the auth0 user_ID

--- christmas-movies/src/components/Profile/index.js
+++ christmas-movies/src/components/Profile/index.js
@@ -3,6 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
+  console.log("user info", user);
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -12,8 +13,8 @@ const Profile = () => {
     isAuthenticated && (
       <div>
         <h2>
-          Ho, Ho, Ho, {user.given_name}. We have the perfect festive film for
-          you!
+          Ho, Ho, Ho, {user.given_name}. Unique_id: {user.sub}. We have the
+          perfect festive film for you!
         </h2>
       </div>
     )
